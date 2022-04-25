cea572d4c7f9539ccf1163b5e9864b736be95d5f
rohan843 <rohan.sharma2934@gmail.com>
2022-04-25 23:03:42 +0530

    Update Code

--- Disk_Scheduling_Algorithms/LOOK.cpp
+++ Disk_Scheduling_Algorithms/LOOK.cpp
@@ -1,10 +1,44 @@
+// Direction towards the larger value
+
 #include <bits/stdc++.h>
 
 using namespace std;
 
 void schedule(int numTracks, int curTrack, vector<int> &queue)
 {
-    
+    priority_queue<int> lesser;
+    priority_queue<int, vector<int>, greater<int>> greater;
+    int cur = curTrack;
+    int trackMovement = 0;
+    for(auto itr: queue)
+    {
+        if(itr >= cur)
+        {
+            greater.push(itr);
+        }
+        else
+        {
+            lesser.push(itr);
+        }
+    }
+    while(!greater.empty())
+    {
+        cout<<cur<<" -> ";
+        int nxt = greater.top();
+        greater.pop();
+        trackMovement += abs(cur - nxt);
+        cur = nxt;
+    }
+    while(!lesser.empty())
+    {
+        cout<<cur<<" -> ";
+        int nxt = lesser.top();
+        lesser.pop();
+        trackMovement += abs(nxt - cur);
+        cur = nxt;
+    }
+    cout<<cur<<endl;
+    cout<<"The number of track movements are: "<<trackMovement<<endl;
 }
 
 int main()
