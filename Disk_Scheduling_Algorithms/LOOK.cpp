6441500835a097943d126fb23615434a1f1c6cbb
rohan843 <rohan.sharma2934@gmail.com>
2022-04-25 22:35:05 +0530

    Add disk scheduling

---
+++ Disk_Scheduling_Algorithms/LOOK.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void schedule(int numTracks, int curTrack, vector<int> &queue)
+{
+    
+}
+
+int main()
+{
+    int numTracks = 200;
+    int curTrack = 50;
+    vector<int> queue = {82, 170, 43, 140, 24, 16, 190};
+    schedule(numTracks, curTrack, queue);
+    return 0;
+}
